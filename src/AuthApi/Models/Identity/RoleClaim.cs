45276a32eee5d19f938f9fe4e5e0c6821e05553a
quang.le <kyquang97@gmail.com>
2020-06-30 13:31:26 +0700

    add new proj, remove build files

---
+++ src/AuthApi/Models/Identity/RoleClaim.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthApi.Models.Identity
+{
+    [Table("AspNetRoleClaims")]
+    public class RoleClaim : IdentityRoleClaim<Guid>
+    {
+    }
+}
---
+++ src/AuthApi/Models/Identity/UserToken.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthApi.Models.Identity
+{
+    [Table("AspNetUserTokens")]
+    public class UserToken: IdentityUserToken<Guid>
+    {
+    }
+}
