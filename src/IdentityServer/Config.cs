c40b3291373440f335177de04d95bff53bb53f60
quang.le <kyquang97@gmail.com>
2020-07-01 13:14:32 +0700

    setup test user

--- src/IdentityServer/Config.cs
+++ src/IdentityServer/Config.cs
@@ -51,10 +51,10 @@ namespace IdentityServer
                     AllowedGrantTypes = GrantTypes.Code,
 
                     // where to redirect to after login
-                    RedirectUris = { "https://localhost:5002/signin-oidc" },
+                    RedirectUris = { "https://localhost:9997/signin-oidc" },
 
                     // where to redirect to after logout
-                    PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
+                    PostLogoutRedirectUris = { "https://localhost:9997/signout-callback-oidc" },
 
                     AllowedScopes = new List<string>
                     {
